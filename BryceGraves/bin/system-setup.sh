84edf7efd84e6289ba46ebffda676f3f2a2d3a81
Bryce Graves <brycegraves97@gmail.com>
2020-01-23 13:45:06 -0800

    SystemSetup: change name to shell file and add todo

---
+++ BryceGraves/bin/system-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# ----------------------
+# Automated System Setup
+# ----------------------
+
+# TODO:
+# 1.  update system since this is assumed to be a fresh install
+# 2.  setup file directories as I like it
+# 3.  download and setup oh-my-zsh
+# 4.  setup rbenv
+# 5.  setup travis cli
+# 6.  download and install discord
+# 7.  download and install telegram
+# 8.  download and install spotify
+# 9.  download and install slack
+# 10. download and install 
