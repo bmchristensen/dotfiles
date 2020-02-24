5c06f20c433f2750001857542422846e4a4c5284
Bryce Graves <brycegraves97@gmail.com>
2020-02-23 21:29:31 -0800

    SystemSetup: updated oh my zsh install paths

--- BryceGraves/bin/system-setup.sh
+++ BryceGraves/bin/system-setup.sh
@@ -156,7 +156,7 @@ echo "
 
 "
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/${USER}/.oh-my-zsh/custom/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 echo "
   ------------------------------------
@@ -172,8 +172,8 @@ echo "
 
 "
 
-git clone https://github.com/zsh-users/zsh-autosuggestions /home/${USER}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/${USER}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 
 echo "
