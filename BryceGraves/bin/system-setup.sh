114da1600579d8e672576f00673046123acb3c85
Bryce Graves <brycegraves97@gmail.com>
2020-02-24 12:25:12 -0800

    SystemSetup: add missing sudo commands for installing vscode insiders

--- BryceGraves/bin/system-setup.sh
+++ BryceGraves/bin/system-setup.sh
@@ -249,8 +249,8 @@ echo "<---- VS Code Insiders ---->
 "
 cd Programs
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install -y code-insiders
 cd
