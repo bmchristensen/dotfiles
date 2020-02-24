ccbbdec44b41d46346ceb05d6d9cd4c61701b8be
Bryce Graves <brycegraves97@gmail.com>
2020-02-23 17:11:53 -0800

    SystemSetup: add missing git install

--- BryceGraves/bin/system-setup.sh
+++ BryceGraves/bin/system-setup.sh
@@ -70,7 +70,7 @@ echo "
 
 "
 
-apt install -y curl dconf-cli uuid-runtime wget ctags vim-scripts neovim python-neovim python3-neovim xclip
+apt install -y curl git dconf-cli uuid-runtime wget ctags vim-scripts neovim python-neovim python3-neovim xclip
 
 echo "
   --------------------
