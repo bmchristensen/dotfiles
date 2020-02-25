cd307b6f0ee97e8b7b8089f137dd972c55d2fc89
Bryce Graves <brycegraves97@gmail.com>
2020-02-24 21:46:29 -0800

    SystemSetup: swap out curl for apt install of chrome

--- BryceGraves/bin/system-setup.sh
+++ BryceGraves/bin/system-setup.sh
@@ -226,10 +226,7 @@ echo "
 echo "<---- Chrome ---->
 
 "
-cd Programs
-wget -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome.deb
-cd
+sudo apt install -y google-chrome-stable
 
 echo "<---- Purging firefox ---->
 
